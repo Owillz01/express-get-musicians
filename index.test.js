24f991a7180c5748772cc81efc3a13af0a23a72f
Owillz01 <omonkhodion@gmail.com>
2024-10-14 11:48:05 +0100

    done with all taska and extensions

--- index.test.js
+++ index.test.js
@@ -5,18 +5,42 @@ execSync('npm run seed');
 
 const request = require("supertest")
 const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
+const { Musician, Band } = require('./models/index')
 const app = require('./src/app');
 const {seedMusician} = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test('can get all musicians from server', async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200); 
+    })
     
+    test('can get single musician from server', async () => {
+        const response = await request(app).get("/musicians/1");
+        expect(response.statusCode).toBe(200); 
+    })
+
+
+
+     
+})
+
+
+describe('./bands endpoint', () => {
+    // Write your tests here
+    test('can get all bands from server', async () => {
+        const response = await request(app).get("/bands");
+        expect(response.statusCode).toBe(200); 
+    })
     
+    // test('can get single musician from server', async () => {
+    //     const response = await request(app).get("/musicians/1");
+    //     expect(response.statusCode).toBe(200); 
+    // })
 
 
 
-
-    
+     
 })
--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
+app.get("/musicians", async (req, res) => {
+    let allMusicians = await Musician.findAll();
+    res.json(allMusicians)
+});
 
+app.get("/musicians/:id", async (req, res) => {
+    console.log(req.params, 'req.params');
+  let musician = await Musician.findByPk(req.params.id);
+  res.json(musician);
+});
 
-
-
+app.get("/bands", async (req, res) => {
+    let allBands = await Band.findAll();
+    console.log(allBands, 'allBands')
+    res.json(allBands)
+});
 
 
 module.exports = app;
